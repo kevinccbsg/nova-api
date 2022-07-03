5ed75ebf9664885410067d3f1ff069c870818472
kevinccbsg <kevinccbsg@gmail.com>
2022-07-04 01:42:52 +0200

    feat: add tests for basic nomination endpoint logic

--- tests/api/api.test.js
+++ tests/api/api.test.js
@@ -1,17 +1,44 @@
 const supertest = require('supertest');
-const startServer  = require('../../app');
+const startServer = require('../../app');
 
 describe('Members endpoints', () => {
   let request;
+  let mongoModels;
+  let dbClient;
   beforeAll(async () => {
-    const { app } = await startServer();
+    const { app, testingHelper, dbInstance } = await startServer();
     request = supertest(app);
+    mongoModels = testingHelper.mongo;
+    dbClient = dbInstance;
+  });
+
+  beforeEach(async () => {
+    await mongoModels.Member.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await dbClient.disconnect();
   });
 
   describe('POST "/api/v1/members/:memberId/nominations"', () => {
-    it('should return 200 OK', () => (
+    it('should return 200 OK and register a new member', () => (
       request.post('/api/v1/members/id/nominations')
-      .expect(200)
+        .send({
+          email: 'eren@titan.com',
+          description: 'has a great power for nova platform',
+          score: {
+            involvement: 8,
+            talent: 10,
+          },
+        })
+        .expect(201)
+        .then(async ({ body }) => {
+          expect(body).toHaveProperty('id');
+          const members = await mongoModels.Member.find();
+          expect(members).toHaveLength(1);
+          const [newMember] = members;
+          expect(newMember).toHaveProperty('id', 'email', 'description', 'talent', 'createdAt', 'updatedAt', 'status');
+        })
     ));
   });
 });
