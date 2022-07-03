3dc6b05e58de2288d210a5ce4950ba347019ee79
kevinccbsg <kevinccbsg@gmail.com>
2022-07-04 00:43:56 +0200

    chore: add eslint config

--- app/index.js
+++ app/index.js
@@ -8,16 +8,16 @@ const app = express();
 
 const startServer = () => new Promise((resolve) => {
   // parse application/x-www-form-urlencoded
-  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.urlencoded({ extended: false }));
   // parse application/json
-  app.use(bodyParser.json())
+  app.use(bodyParser.json());
   app.use(helmet({
     contentSecurityPolicy: false,
   }));
   app.use(compression());
   app.use(morgan('tiny', { skip: () => process.env.NODE_ENV === 'test' }));
 
-  return resolve({ app });
+  resolve({ app });
 });
 
 module.exports = startServer;
